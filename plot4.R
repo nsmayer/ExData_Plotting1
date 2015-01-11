1a1b593d8253c0d5898f4c5e1ce375b41c9dfa78
Nathan Mayer <nathan@Uriel.local>
2015-01-11 16:04:33 -0600

    Plot and script for first programing assignment.  Plot4

---
+++ plot4.R
@@ -0,0 +1,29 @@
+#!/usr/bin Rscript
+
+#R code for plot1
+
+#Name the columns since I'm only loading in the relevent data
+col_names <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+#Load only the relevent data (Febuary 1st and 2nd 2007)  Discovered using grep, head, tail at unix command line
+df <- read.table("household_power_consumption.txt", sep = ";", na.strings = "?", nrows = 2880, skip = 66637, col.names = col_names)
+
+date <- as.POSIXct((strptime(paste(df$Date, df$Time, sep = " "), "%d/%m/%Y %H:%M:%S")))
+
+#Set up the plot output file
+png("plot4.png", width = 480, height = 480)
+par(mfrow=c(2, 2))
+plot(date, df$Global_active_power, type = "l", ylab = "Global Active Power", xlab="")
+plot(date, df$Voltage, type = "l", ylab = "Global Active Power", xlab="datetime")
+
+plot(date, df$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab="")
+lines(date, df$Sub_metering_2, col = 2)
+lines(date, df$Sub_metering_3, col = 4)
+legend(x = 1170380080 , y = 39.5, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1, 1), col = c("black","red", "blue"), cex = 0.85, bty = "n")
+
+plot(date, df$Global_reactive_power, type = "l", ylab = "Global Active Power", xlab="datetime")
+
+dev.off()
+
+
+
